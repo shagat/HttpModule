ca557dc0d45016a81795e4983fae57e41be0e4be
shagat <shagat.bing@gmail.com>
2022-06-10 15:39:22 +0530

    other parameters of a http requests and interceptors introduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { AuthInterceptorService } from './auth-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,13 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from '@angular/common/http';
-import { catchError, map, Subject, throwError } from 'rxjs';
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from '@angular/common/http';
+import { catchError, map, Subject, tap, throwError } from 'rxjs';
 
 import { Post } from './post.model';
 
@@ -15,42 +15,68 @@ export class PostsService {
         const postData: Post = { title: title, content: content };
         this.http
             .post<{ name: string }>('https://ng-complete-guide-5f3a6-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json',
-                postData)
-                .subscribe({
-                        next: (responseData) => console.log(responseData),
-                        error: (e) => this.error.next(e),
-                        complete: () => console.info('complete')
-                    });
-                    // .subscribe(responseData => {
-                    //     console.log(responseData);
-                    // }, error => {
-                    //     this.error.next(error.message)
-                    // });
+                postData,
+                {
+                    observe: 'response'
+                })
+            .subscribe({
+                next: (responseData) => console.log(responseData),
+                error: (e) => this.error.next(e),
+                complete: () => console.info('complete')
+            });
+        // .subscribe(responseData => {
+        //     console.log(responseData);
+        // }, error => {
+        //     this.error.next(error.message)
+        // });
     }
 
 
 
     fetchPosts() {
+        let searchParams = new HttpParams();
+        searchParams = searchParams.append('print', 'pretty');
+        searchParams = searchParams.append('custom', 'key');
         return this.http
-            .get<{ [key: string]: Post }>('https://ng-complete-guide-5f3a6-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
-            .pipe(map(responseData => {
-                const postsArray: Post[] = [];
-                for (const key in responseData) {
-                    if (responseData.hasOwnProperty(key)) {
-                        postsArray.push({ ...responseData[key], id: key });
-                    }
-                }
-                return postsArray;
-            }), catchError(errorRes => {
-                //send to analytics server
-                return throwError(() => {
-                    this.error.next(errorRes);
-                });
+            .get<{ [key: string]: Post }>('https://ng-complete-guide-5f3a6-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json', {
+                headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+                // params: new HttpParams().set('print', 'pretty')
+                params: searchParams,
+                responseType: 'json' //just for note, this is defaulted.
             })
+            .pipe(
+                map(responseData => {
+                    const postsArray: Post[] = [];
+                    for (const key in responseData) {
+                        if (responseData.hasOwnProperty(key)) {
+                            postsArray.push({ ...responseData[key], id: key });
+                        }
+                    }
+                    return postsArray;
+                }), catchError(errorRes => {
+                    //send to analytics server
+                    return throwError(() => {
+                        this.error.next(errorRes);
+                    });
+                })
             );
     }
 
     deletePosts() {
-        return this.http.delete('https://ng-complete-guide-5f3a6-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json');
+        return this.http.delete('https://ng-complete-guide-5f3a6-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json', {
+            observe: 'events',
+        }
+        ).pipe(
+            tap(
+                event => {
+                    console.log(event);
+                    if (event.type === HttpEventType.Sent){
+                        //....
+                    }
+                    if (event.type === HttpEventType.Response){
+                        console.log(event.body);
+                    }
+                }
+            ));
     }
 }
